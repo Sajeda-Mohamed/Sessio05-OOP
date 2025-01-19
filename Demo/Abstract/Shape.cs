bacabc4f471f6067d592c2b6db1345c1a4c99f3e
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-19 16:57:42 +0200

    Add project files.

---
+++ Demo/Abstract/Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Abstract
+{
+    internal abstract class Shape
+    {
+        public double Hight { get; set; }
+        public double Width { get; set; }
+        public abstract double Parimeter { get; }
+        public abstract double CalcArea();
+    }
+    internal class Rectanglee : RectBase 
+    {
+        public override double Parimeter { get { return (Hight + Width) * 2; } }
+    }
+    internal class Square : RectBase
+    {
+        public Square(double Dim)
+        {
+            Hight = Width = Dim;
+        }
+        public override double Parimeter { get { return Width * 4; } }
+    }
+    abstract class RectBase : Shape
+    {
+        public override double CalcArea()
+        {
+            return Hight * Width;
+        }
+    }
+    class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            radius = Width = Hight;
+        }
+        public override double Parimeter { get { return 2 * 3.14 * Width; } }
+        public override double CalcArea()
+        {
+            return 3.14 * Math.Pow(Width,2);
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Demo.Abstract;
+using System.Drawing;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Abstract
+            //Rectanglee rectangle = new Rectanglee();
+
+            //double rectArea = rectangle.CalcArea();
+
+            //double rectParimeter = rectangle.Parimeter;
+
+            //Square square = new Square(10);
+
+            //double sqParimeter = square.Parimeter;
+
+            //double sqArea = square.CalcArea();
+
+            //Circle circle = new Circle(10);
+
+            //double circleParimeter = circle.Parimeter;
+
+            //double circleArea = circle.CalcArea();
+            #endregion
+
+        }
+    }
+}
