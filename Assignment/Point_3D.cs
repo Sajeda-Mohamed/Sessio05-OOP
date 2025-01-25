1ee63c255eed29a7517201ccf5f49208679031e0
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-01-25 16:28:31 +0200

    First Project

---
+++ Assignment/Point_3D.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Point_3D : ICloneable,IComparable
+    {
+        #region Auto Prop
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z{ get; set; }
+        #endregion
+
+        #region Constractors
+        public Point_3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y; 
+            Z = z;
+        }
+        public Point_3D()
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Point Coordinates:({X},{Y},{Z})";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Point_3D passedPoint = obj as Point_3D;
+            if (this.X > passedPoint.X && this.Y > passedPoint.Y)
+                return 1;
+            else if (this.X < passedPoint.X && this.Y < passedPoint.Y)
+                return -1;
+            else
+                return 0;
+        }
+
+        public object Clone()
+        {
+            return new Point_3D()
+            {
+                X = this.X,
+                Y = this.Y,
+                Z = this.Z,
+            };
+        }
+
+        public static bool operator ==(Point_3D left, Point_3D right)
+        {
+            if (left.X == right.X && left.Y == right.Y && left.Z == right.Z)
+                return true;
+            else
+                return false;
+        } 
+        public static bool operator !=(Point_3D left, Point_3D right)
+        {
+            if (left.X != right.X || left.Y != right.Y || left.Z != right.Z)
+                return true;
+            else
+                return false;
+        }
+        #endregion
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First Project
+            //Point_3D P = new Point_3D(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+            #region point_1
+            Console.WriteLine("Enter x1 : ");
+            bool flag1 = double.TryParse(Console.ReadLine(), out double x1);
+            Console.WriteLine("Enter y1 : ");
+            bool flag2 = double.TryParse(Console.ReadLine(), out double y1);
+            Console.WriteLine("Enter z1 : ");
+            bool flag3 = double.TryParse(Console.ReadLine(), out double z1);
+            #endregion
+
+            #region Point_2
+            Console.WriteLine("Enter x2 : ");
+            bool flag4 = double.TryParse(Console.ReadLine(), out double x2);
+            Console.WriteLine("Enter y2 : ");
+            bool flag5 = double.TryParse(Console.ReadLine(), out double y2);
+            Console.WriteLine("Enter z2 : ");
+            bool flag6 = double.TryParse(Console.ReadLine(), out double z2);
+            #endregion
+
+            Point_3D P1 = new Point_3D(x1, y1, z1);
+            Point_3D P2 = new Point_3D(x2, y2, z2);
+
+            if(P1 == P2)
+                Console.WriteLine("P1 equal P2");
+            else
+                Console.WriteLine("P1 not equal P2");
+
+            P2 =(Point_3D) P1.Clone();
+            Console.WriteLine(P2);
+
+            #region Array of Points
+            Point_3D[] Points  =
+            {
+                new Point_3D() {X = 1, Y = 2, Z = 3},
+                new Point_3D() {X = 4, Y = 5, Z = 6},
+                new Point_3D() {X = 7, Y = 8, Z = 9}
+            };
+
+            Array.Sort(Points);
+            foreach (var item in Points)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+            #endregion
+        }
+    }
+}
